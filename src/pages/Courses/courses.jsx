c6314e044fbb537bdf0f610df5d3fe05a395b931
Loïc Messela <messelaloic@gmail.com>
2023-03-14 11:58:55 +0100

    Saving old files

--- src/pages/Courses/courses.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const courses = () => {
-    return (
-        <h1>courses</h1>
-    )
-}
-
-export default courses
--- src/pages/Teachers/teachers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Teachers = () => {
-    return (
-        <h1>teachers</h1>
-    )
-}
-
-export default Teachers
