9be5489fe200532d9f9d9e018a354c5edcc19412
Vishnu <svishnu987@gmail.com>
2023-03-16 10:05:38 +0100

    Add: CSS changes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 function AuthContextWrapper(props) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   function storeToken(receivedToken) {
     localStorage.setItem("token", receivedToken);
