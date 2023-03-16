5a4e2445c60dd1fb5bb90c5b724b3e0d465e867a
Vishnu <svishnu987@gmail.com>
2023-03-16 11:01:30 +0100

    fix: service path

--- src/service/service.js
+++ src/service/service.js
@@ -13,25 +13,24 @@ const myApi = axios.create({
 // Teachers :
 
 myApi.getAllTeachers = (queryString) => {
-  return myApi.get(`/teachers?${queryString}`);
+  return myApi.get(`/api/teachers?${queryString}`);
 };
 
 myApi.getOneTeacher = (id) => {
-  return myApi.get(`/teachers/${id}`);
+  return myApi.get(`/api/teachers/${id}`);
 };
 
 myApi.favTeacher = (id) => {
-  return myApi.post(`/favorite-teacher/${id}/add`);
-}
+  return myApi.post(`/api/favorite-teacher/${id}/add`);
+};
 
 myApi.getfavoriteTeacher = () => {
-  return myApi.get(`/favorite-teacher`)
-}
+  return myApi.get(`/api/favorite-teacher`);
+};
 
 myApi.removedTeachers = (id) => {
-  return myApi.delete(`/favorite-teacher/${id}`)
-}
-
+  return myApi.delete(`/api/favorite-teacher/${id}`);
+};
 
 //Courses :
 myApi.interceptors.request.use((request) => {
@@ -41,35 +40,35 @@ myApi.interceptors.request.use((request) => {
 });
 
 myApi.createCourse = (course) => {
-  return myApi.post("/", course);
+  return myApi.post("/api/courses/add", course);
 };
 
 myApi.getAllCourses = (queryString) => {
-  return myApi.get(`/courses?${queryString}`);
+  return myApi.get(`/api/courses?${queryString}`);
 };
 
 myApi.getOneCourse = (id) => {
-  return myApi.get(`/courses/${id}`);
+  return myApi.get(`/api/courses/${id}`);
 };
 
 myApi.deleteCourse = (id) => {
-  return myApi.delete(`/courses/${id}`);
+  return myApi.delete(`/api/courses/${id}`);
 };
 
 myApi.favoriteCourses = (id) => {
-  return myApi.post(`/favorite-courses/${id}/add`);
-}
+  return myApi.post(`/api/favorite-courses/${id}/add`);
+};
 
 myApi.getfavoriteCourses = () => {
-  return myApi.get(`/favorite-courses`)
-}
+  return myApi.get(`/api/favorite-courses`);
+};
 
 myApi.removedCourse = (id) => {
-  return myApi.delete(`/favorite-courses/${id}/remove`)
-}
+  return myApi.delete(`/api/favorite-courses/${id}/remove`);
+};
 
 myApi.updateCourse = (id, course) => {
-  return myApi.patch(`/courses/${id}`, course);
+  return myApi.patch(`/api/courses/${id}`, course);
 };
 
 export default myApi;
