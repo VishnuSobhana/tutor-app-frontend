b76467c9a0b5a894ebea2133aea1cd01bcf4e620
Loïc Messela <messelaloic@gmail.com>
2023-03-16 16:49:36 +0100

    AuthContext, AddCourse, EditCourse, Home

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,53 +1,53 @@
-import { createContext, useState, useEffect } from "react";
-import myApi from "./../service/service";
-export const AuthContext = createContext();
+import { createContext, useState, useEffect } from 'react'
+import myApi from './../service/service'
+export const AuthContext = createContext()
 
 function AuthContextWrapper(props) {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   function storeToken(receivedToken) {
-    localStorage.setItem("token", receivedToken);
-    setToken(receivedToken);
+    localStorage.setItem('token', receivedToken)
+    setToken(receivedToken)
   }
 
   function getToken() {
-    return localStorage.getItem("token");
+    return localStorage.getItem('token')
   }
 
   function removeToken() {
-    localStorage.removeItem("token");
+    localStorage.removeItem('token')
   }
 
   async function authenticateUser() {
     try {
-      const currentToken = getToken();
-      setToken(currentToken);
-      // if (!currentToken) return setUser(null);
+      const currentToken = getToken()
+      setToken(currentToken)
+      if (!currentToken) {
+        setUser(null)
+        setIsLoading(false)
+        return
+      }
 
-      const response = await myApi.get("/auth/me", {
+      const response = await myApi.get('/auth/me', {
         headers: {
           Authorization: `Bearer ${currentToken}`,
         },
-      });
-      if (response.status === 200) {
-        setUser(response.data);
-        setIsLoading(false);
-      } else {
-        setUser(null);
-        setIsLoading(false);
-      }
+      })
+
+      setUser(response.data)
+      setIsLoading(false)
     } catch (error) {
-      console.log(error);
-      setUser(null);
-      setIsLoading(false);
+      console.log(error)
+      setUser(null)
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
-    authenticateUser();
-  }, []);
+    authenticateUser()
+  }, [])
 
   return (
     <AuthContext.Provider
@@ -55,7 +55,7 @@ function AuthContextWrapper(props) {
     >
       {props.children}
     </AuthContext.Provider>
-  );
+  )
 }
 
-export default AuthContextWrapper;
+export default AuthContextWrapper
